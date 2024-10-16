98d2b3ebe5ec0a33078a4c719998419d0bdf7d41
AmitSh1008 <aamitshilman1008@gmail.com>
2024-10-16 17:17:23 +0300

    Inital Commit

---
+++ wanna-join/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AnimatedForm from './Components/AnimatedForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AnimatedForm />
+    </div>
+  );
+}
+
+export default App;
